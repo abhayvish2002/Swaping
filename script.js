a86f9dda2285c943fc2ba70b3ab949a703c67e05
Abhay vishwakarma <akvishwakarma25082002@gmail.in>
2025-03-08 20:49:28 +0530

    js changes

--- script.js
+++ script.js
@@ -1,36 +1,17 @@
+document.getElementById("btn").addEventListener('click', function () {
+    let input1 = document.getElementById('input1').value;
+    let input2 = document.getElementById('input2').value;
 
-
-document.getElementById("btn").addEventListener('click', function() {
-    let input1=document.getElementById('input1').value;
-    let input2=document.getElementById('input2').value;
-
-    if(input1 === ""  || input2 === ""){
-document.getElementById('ab').textContent = "enter number";
-return;
+    if (input1 === "" || input2 === "") {
+        document.querySelector('.ab').textContent = "please enter number in the input box";
+        
+        return;
     }
+
+    let tem=input1;
+        input1=input2;
+        input2=tem;
+
+        document.getElementById('input1').value=input1;
+        document.getElementById('input2').value=input2;
 });
-
-
-
-
- 
-
-
-
-// document.getElementById('swapBtn').addEventListener('click', function() {
-//     let num1 = document.getElementById('num1').value;
-//     let num2 = document.getElementById('num2').value;
-
-//     if (num1 === "" || num2 === "") {
-//         document.getElementById('result').textContent = "Please enter both numbers!";
-//         return;
-//     }
-
-//     // Swap logic using destructuring assignment
-//     [num1, num2] = [num2, num1];
-
-//     document.getElementById('num1').value = num1;
-//     document.getElementById('num2').value = num2;
-
-//     document.getElementById('result').textContent = `Numbers swapped! Now Number 1 is ${num1} and Number 2 is ${num2}.`;
-// });
